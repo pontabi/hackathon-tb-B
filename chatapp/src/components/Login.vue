22d5be06777e4ad5232e8f95ae9c685ecce09214
aoba-s <s1f102100614@iniad.org>
2023-08-22 17:11:52 +0900

    入室時の処理実装

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject, ref } from "vue"
+import { defineCustomElement, inject, ref } from "vue"
 import { useRouter } from "vue-router"
 import io from "socket.io-client"
 
@@ -20,10 +20,13 @@ const inputUserName = ref("")
 // 入室メッセージをクライアントに送信する
 const onEnter = () => {
   // ユーザー名が入力されているかチェック
+  if(!inputUserName.value) return;
 
   // 入室メッセージを送信
+  socket.emit("enterEvent", "入室しました")
 
   // 全体で使用するnameに入力されたユーザー名を格納
+  userName.value = inputUserName.value
 
   // チャット画面へ遷移
   router.push({ name: "chat" })
@@ -36,7 +39,7 @@ const onEnter = () => {
     <h1 class="text-h3 font-weight-medium">Vue.js Chat サンプル</h1>
     <div class="mt-10">
       <p>ユーザー名</p>
-      <input type="text" class="user-name-text" />
+      <input type="text" class="user-name-text" v-model="inputUserName"  />
     </div>
     <button type="button" @click="onEnter" class="button-normal">入室する</button>
   </div>
