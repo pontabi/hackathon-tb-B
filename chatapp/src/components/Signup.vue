866e175de8992976f779cd1895a061a25d3b4a97
pontabi <jp.takapon@gmail.com>
2023-09-05 13:51:10 +0900

    Signup UI改善

--- chatapp/src/components/Signup.vue
+++ chatapp/src/components/Signup.vue
@@ -1,91 +1,122 @@
-<template>
-  <form @submit.prevent="">
-    <div class="container d-flex align-items-center">
-      <v-card class="mx-auto my-5 p-5 signup-card" >
-        <h1 class="text-h3 font-weight-medium my-5">ChatApp Signup</h1>
-        <div class="mt-10">
-          <v-text-field
-            v-model="inputUserName"
-            @keydown.enter="onEnter"
-            @compositionstart="onIMEStart"
-            @compositionend="onIMEEnd"
-            class=""
-            type="text"
-            placeholder="username"
-          />
-          <v-text-field
-            v-model="inputUserEmail"
-            @keydown.enter="onEnter"
-            @compositionstart="onIMEStart"
-            @compositionend="onIMEEnd"
-            class=""
-            type="email"
-            placeholder="email"
-          />
-          <v-text-field
-            v-model="inputPassword"
-            @keydown.enter="onEnter"
-            @compositionstart="onIMEStart"
-            @compositionend="onIMEEnd"
-            class=""
-            type="password"
-            placeholder="password"
-          />
-        </div>
-        <v-btn type="button" @click="onEnter" class="w-100" color="primary">入室する</v-btn>
-        <router-link to="/signup" class="signup-link">Signup</router-link>
-      </v-card>
-    </div>
-  </form>
-</template>
-
 <script setup>
 import { inject, onMounted, ref } from "vue"
 import { useRouter } from "vue-router"
 import io from "socket.io-client"
-
-// #region global state
-const currentUser = inject("currentUser")
-const chatList = inject("chatList")
-const userList = inject("userList")
-// #endregion
+import { sha256 } from 'js-sha256'
+import { useField, useForm } from 'vee-validate'
 
 // #region local variable
 const router = useRouter()
 const socket = io()
 // #endregion
 
-// #region reactive variable
-const inputUserName = ref("")
-const inputUserEmail = ref("")
-const isIMEActive = ref(false)
-// #endregion
-
-const onEnter = () => {
-  // 日本語変換中のkeydown.enterイベントを回避
-  if (isIMEActive.value) return
-  console.log(inputUserName.value)
-}
-// 日本語変換中のkeydown.enterイベントを回避
-const onIMEStart = () => {
-  isIMEActive.value = true
-}
-const onIMEEnd = () => {
-  isIMEActive.value = false
-}
 // #region lifecycle
 onMounted(() => {
-  registerSocketEvent()
+  // signupイベントを受け取ったら実行
+  socket.on("signupEvent", (newUser) => {
+
+  })
 })
+// #endregion
+
+
+// #region form handler
+const { handleSubmit, handleReset } = useForm({
+  // 入力情報のバリデーション
+  validationSchema: {
+    name (value) {
+      if (value?.length >= 2) return true
+
+      return '2文字以上入力してください'
+    },
+    email (value) {
+      if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+
+      return '正しいe-mailを入力してください'
+    },
+    password (value) {
+      if (value?.length >= 6) return true
+
+      return '6文字以上入力してください'
+    },
+  },
+})
+
+// input-fieldとのバインディング
+const name = useField('name')
+const email = useField('email')
+const password = useField('password')
+
+// submit成功時の処理
+const submit = handleSubmit(values => {
+  // パスワードをsha256でハッシュ化
+  const password = sha256(values.password)
+
+  const newUser = {
+    name: values.name,
+    email: values.email,
+    password: password
+  }
+
+  // signupEventを送信
+  socket.emit("signupEvent", newUser)
+})
+// #endregion
 </script>
+
+<template>
+  <v-card class="signup-card">
+    <h1 class="text-center mb-5">ChatApp Signup</h1>
+    <form @submit.prevent="submit">
+      <v-text-field
+        v-model="name.value.value"
+        :counter="10"
+        :error-messages="name.errorMessage.value"
+        label="Name"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email.value.value"
+        :error-messages="email.errorMessage.value"
+        label="E-mail"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="password.value.value"
+        :error-messages="password.errorMessage.value"
+        type="password"
+        label="Password"
+      ></v-text-field>
+
+      <div class="d-flex justify-end">
+        <v-btn
+          class="me-4 test-right"
+          type="submit"
+        >
+          submit
+        </v-btn>
+
+        <v-btn
+          class="text-right"
+          @click="handleReset"
+        >
+          clear
+        </v-btn>
+      </div>
+    </form>
+  </v-card>
+</template>
 
 <style scoped>
 .signup-card {
   width: 500px;
-}
-.signup-link {
-  text-decoration: none;
-  display: block;
-  text-align: right;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  height: 500px;
+  padding: 5rem;
 }
 </style>
